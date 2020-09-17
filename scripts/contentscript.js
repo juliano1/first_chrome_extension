30ba339c5ace29a2cf6a003a5c6919de1575bbef
Juliano <julianopaulo@gmail.com>
2020-09-17 20:11:18 -0300

    first commit

---
+++ scripts/contentscript.js
@@ -0,0 +1,5 @@
+console.log('Content script');
+let topDiv = document.createElement('div');
+topDiv.setAttribute('class', 'content-top-div');
+topDiv.innerText = 'This div is added by My first chrome extension.';
+document.body.appendChild(topDiv);
